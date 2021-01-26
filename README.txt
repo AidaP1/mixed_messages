798c46abd4caa388deb1f4890b4ff8c10aa4b5fa
Aidan Pilgrim <aidan@Aidans-MacBook-Air.local>
2021-01-26 20:46:06 +0000

    First round of the project with readme

---
+++ README.txt
@@ -0,0 +1,22 @@
+Are you a busy boss who can't stand hearing your employees go on about personal development? Isn't it enough that you let them work here?
+
+We know you have other things to focus on, like golf, and sucking up to *your* boss, so we've made this Performance Review Randomizer just for you. Spent too much time 'relationship building' with clients to actually turn up at the office? Doesn't matter. Don't know what your team got up to in the last 3 months? Irrelevant. Their goals for the next 3 months? Who cares.
+
+As a busy boss I want to be able to generate a believable performance review without inputting any information about the person. This will allow me to run one for each report very quickly.
+
+As a busy boss, I rely on my favorite employees to cover my ass, I need to make sure they get good performance reviews and metion them by name, so that they'll continue to make me look good.
+
+As a busy boss, I need to be able to give some of my more annoying reports bad reviews. This will help me generate a hostile work environment towards anyone in my team who questions me, and drive them out.
+
+As a busy boss I need the review generator to reference non-specific work and projects from my reports, so that they think I'm paying attention to their effort.
+
+OPTIONAL: As a busy boss I haven't got time to review comp and benefits. I need the randomizer to assign a % pay increase that seems reasonable based on the feedback. This will save me the hassle of working it out and sending it to finance.
+
+
+INSTRUCTIONS:
+A performance review is generated with the `fullReview()` function, which takes two optional parameters:
+- name, leave blank for a random, yet endearing, nickname
+- rating, takes either 'good' or 'bad' as a string and will output either a good or a bad review. Leave blank for a random review
+- leaving both blank will generate a good or bad review to a non-specific person
+
+NB: if you can't remember the name, but you want to give them a good or bad review, pass '???' as the name argument, followed by the rating parameter.
---
+++ main.js
@@ -0,0 +1,75 @@
+const blocks = { // object contains all of the information that will be pulled into various reviews
+    nameArr : ['Sport','Slugger','Champ'],
+    subjects : ['clients','peers','other managers','the board'],
+    goodBehaviours : ['great focus','fantastic relationship building','ability to think on your feet'],
+    badBehaviours : ['poor attention to detail','standoff-ish manner','inability to accept constructive feedback'],
+    projects : ['critical client requests','key strategic deliverables','leveraging synergies across our key parts of our tech stack to deliver efficiency through best practice'],
+    projectFeedbackGood : ['quick turnaround times','focus on quick delivery','great project management'],
+    projectFeedbackBad : ['sloppy testing approach','failure to hit deadlines','lack of value-add contributions'],
+    overallGood : ['you are exactly the type of person that will drive this company\'s growth','keep doing the great stuff that you\'re doing','we really value your contribution'],
+    overallBad : ['you need to seriously consider your position in this organisation','we\'re very disappointed in your work','we need you to step it up'],
+  }
+  
+  
+  function fillName (name) { //checks if the name is known or blank
+    if (name === undefined || name === '???') { // name === '???' allows definintion off good/bad reviews even if you don't know the reviwee's name
+      name = blocks.nameArr[Math.floor(Math.random() * blocks.nameArr.length)]
+    }
+    return name
+  }
+  function fillRating (rating) { // checks for a good or bad rating and randomly assigns one if it doesnt exist, or has been incorrectly filled
+    if (rating === undefined) {
+      rating = 'unknown'
+    }
+    switch (rating.toLowerCase()) {
+      case 'good' :
+      return 'good';
+      case 'bad' :
+      return 'bad';
+      default :
+      return Math.floor(Math.random() * 2) === 0 ? 'good' : 'bad';
+    }
+  }
+  function assembleGoodReview () { //pulls a selection of text items into a review string
+    const behaviourSubject = blocks.subjects[Math.floor(Math.random() * blocks.subjects.length)];
+    const behaviour = blocks.goodBehaviours[Math.floor(Math.random() * blocks.goodBehaviours.length)];
+    const projSubject = blocks.subjects[Math.floor(Math.random() * blocks.subjects.length)];
+    const project = blocks.projects[Math.floor(Math.random() * blocks.projects.length)];
+    const projFB = blocks.projectFeedbackGood[Math.floor(Math.random() * blocks.projectFeedbackGood.length)];
+    const overallFB = blocks.overallGood[Math.floor(Math.random() * blocks.overallGood.length)];
+    const review = `After careful examination of your performance, including conversations with stakeholders across the organisation, a number of pieces of feedback have stood out. In particular, ${behaviourSubject} noted your ${behaviour} throughout the quarter. With regards to specific work; whilst working on ${project}, ${projSubject} called out your ${projFB}. Overall; ${overallFB}.`
+    return review;
+  }
+  function assembleBadReview () { //pulls a selection of text items into a review string
+    const behaviourSubject = blocks.subjects[Math.floor(Math.random() * blocks.subjects.length)];
+    const behaviour = blocks.badBehaviours[Math.floor(Math.random() * blocks.badBehaviours.length)];
+    const projSubject = blocks.subjects[Math.floor(Math.random() * blocks.subjects.length)];
+    const project = blocks.projects[Math.floor(Math.random() * blocks.projects.length)];
+    const projFB = blocks.projectFeedbackBad[Math.floor(Math.random() * blocks.projectFeedbackBad.length)];
+    const overallFB = blocks.overallBad[Math.floor(Math.random() * blocks.overallGood.length)];
+    const reviewBody = `After careful examination of your performance, including conversations with stakeholders across the organisation, a number of pieces of feedback have stood out. In particular, ${behaviourSubject} noted your ${behaviour} throughout the quarter. With regards to specific work; whilst working on ${project}, ${projSubject} called out your ${projFB}. Overall; ${overallFB}.`
+    return reviewBody;
+  }
+  
+  function fullReview(name,rating) { // main function
+    let revName = fillName(name);
+    let revRate = fillRating(rating) //chooses either a good or bad review
+    const review = { //stores the review as an object
+      reviewText : '',
+      reviewGrade : 0,
+    }
+    if (revRate === 'good') { //chooses and fills the review
+      review.reviewText = `Howdy ${revName}. ${assembleGoodReview()}`
+      review.reviewGrade = `${Math.floor(Math.random() * 2) + 4} / 5` //assign a grade out of 5 (either 4 or 5)
+    } else {
+      review.reviewText = `Howdy ${revName}. ${assembleBadReview()}`
+      review.reviewGrade = `${Math.floor(Math.random() * 2) + 1} / 5` //assign a grade out of 5 (either 1 or 2)
+    }
+    return review;
+  }
+  
+  
+  
+  //Run code below this line
+  
+  console.log(fullReview())
